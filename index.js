cc177c594e1de69786a1a860a04b63a3cff89fd4
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-04-10 21:59:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import qr from "qr-image";
+import fs from "fs";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.static('styles'));
+
+let qrURL = "";
+
+function generateQRCode() {
+  if (qrURL) {
+    const qr_png = qr.imageSync(qrURL, { type: "png" });
+    fs.writeFileSync("public/qr_img.png", qr_png);
+
+    fs.writeFile("public/URL.txt", qrURL, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  }
+}
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { qrURL: qrURL });
+});
+
+app.get("/qr_img.png", (req, res) => {
+  res.sendFile(`${__dirname}/public/qr_img.png`);
+});
+
+app.post("/submit", (req, res) => {
+  qrURL = req.body.URL;
+  generateQRCode();
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
