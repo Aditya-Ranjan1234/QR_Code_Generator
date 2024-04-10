2a66deeeae63f6912997ae20bd71a4b4cab79cd7
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-04-10 22:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.static('styles'));
 
+const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
 let qrURL = "";
 
 function generateQRCode() {
